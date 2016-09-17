592a612237da462443680ed82d8f31b94cbe444b
NanoAi <tenshi@mirai.red>
2016-09-17 05:04:40 -0400

    Re: Nightly Fix
    This should make damage blocking work.

--- lua/apg/modules/misc.lua
+++ lua/apg/modules/misc.lua
@@ -60,4 +60,15 @@ APG.timerRegister( mod, "APG_autoFreeze", APG.cfg["autoFreezeTime"].value, 0, fu
     end
 end)
 
+--[[------------------------------------------
+        Load hooks and timers
+]]--------------------------------------------
+for k, v in next, APG[mod]["hooks"] do
+    hook.Add( v.event, v.identifier, v.func )
+end
+
+for k, v in next, APG[mod]["timers"] do
+    timer.Create( v.identifier, v.delay, v.repetitions, v.func )
+end
+
 
